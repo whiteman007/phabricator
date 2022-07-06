0c5d3f9939fecf4257728611e6cebfd573f73e8b
whiteman007 <108463448+whiteman007@users.noreply.github.com>
2022-07-06 07:53:17 -0400

    Add files via upload

---
+++ query/PhabricatorNamedQueryConfigQuery.php
@@ -0,0 +1,60 @@
+<?php
+
+final class PhabricatorNamedQueryConfigQuery
+  extends PhabricatorCursorPagedPolicyAwareQuery {
+
+  private $ids;
+  private $engineClassNames;
+  private $scopePHIDs;
+
+  public function withIDs(array $ids) {
+    $this->ids = $ids;
+    return $this;
+  }
+
+  public function withScopePHIDs(array $scope_phids) {
+    $this->scopePHIDs = $scope_phids;
+    return $this;
+  }
+
+  public function withEngineClassNames(array $engine_class_names) {
+    $this->engineClassNames = $engine_class_names;
+    return $this;
+  }
+
+  public function newResultObject() {
+    return new PhabricatorNamedQueryConfig();
+  }
+
+  protected function buildWhereClauseParts(AphrontDatabaseConnection $conn) {
+    $where = parent::buildWhereClauseParts($conn);
+
+    if ($this->ids !== null) {
+      $where[] = qsprintf(
+        $conn,
+        'id IN (%Ld)',
+        $this->ids);
+    }
+
+    if ($this->engineClassNames !== null) {
+      $where[] = qsprintf(
+        $conn,
+        'engineClassName IN (%Ls)',
+        $this->engineClassNames);
+    }
+
+    if ($this->scopePHIDs !== null) {
+      $where[] = qsprintf(
+        $conn,
+        'scopePHID IN (%Ls)',
+        $this->scopePHIDs);
+    }
+
+    return $where;
+  }
+
+  public function getQueryApplicationClass() {
+    return 'PhabricatorSearchApplication';
+  }
+
+}
